527585eb5bfb53be7bd238f8bafc6fa306c40b8a
denison123 <denison2908@gmail.com>
2021-02-20 17:26:38 -0300

    conexao com o banco de dados

--- views/teste/index.js
+++ views/teste/index.js
@@ -1,4 +1,28 @@
-var nome = "grupo"
-var sobre = "positando"
-var junc = nome + sobre
-console.log(junc)
+const Sequelize = require ('sequelize')
+const { DEFAULT_MIN_VERSION } = require('tls')
+const sequelize = new Sequelize('materiais','root','',{
+    host:'localhost',  
+    dialect:'mysql'
+})
+sequelize.authenticate().then(function(){
+    console.log('conectado com sucesso')
+})
+.catch(function(erro){
+    console.log('Falha na conexao: ' + erro)
+})
+// adicionando tabela em banco de dados 
+// const denison = sequelize.define('denison',{
+//     titulo: {
+//         type: Sequelize.STRING
+//     },
+//     conteudo:{
+//         type: Sequelize.TEXT
+//     }
+//})
+// adicionando dados na tala
+// denison.create({
+//     titulo: 'lopes',
+//     conteudo:'rocha',
+//     })
+// //denison.sync({force:true})
+
